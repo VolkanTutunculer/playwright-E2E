c112731b7e100b46a49b7afa91ee1c4ff904e365
volkan.tutunculer <volkan.tutunculer@gmail.com>
2025-05-27 21:53:11 -0400

    first update

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,29 @@
 import { test, expect } from '@playwright/test';
 
-test('has title @Smoke', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.describe('Demo @Smoke', () => {
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  test.beforeEach(async({ page }) => {
+    await page.goto('https://playwright.dev/');
+  });
+
+  test('has titl', async ({ page }) => {
+    // Expect a title "to contain" a substring.
+    await expect(page).toHaveTitle(/Playwright/);
+  });
+  
+  test('get started link', async ({ page }) => {
+    // Click the get started link.
+    await page.getByRole('link', { name: 'Get started' }).click();
+  
+    // Expects page to have a heading with the name of Installation.
+    await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  });  
 });
 
-test('get started link @Smoke', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
+/*
+Go to https://www.wikipedia.org/
+Search for "Playwright"
+Validate the url contains "Playwright"
+Validate the title contains "Playwright"
+Validate the main heading is "Playwright"
+*/
